b9a39aaab9c774b3dae6cae4e1b2c97f49ef492f
jacklinekerketta <jackline.kerketta@practo.com>
2026-02-11 19:50:59 +0530

    Update API base URL to use environment variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   withCredentials: true, // session cookies
 });
+
